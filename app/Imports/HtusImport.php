9f86ae767cabb03b7b468467007849bb18c12140
Sonu Singh <sonu.singh@pairroxz.com>
2022-09-09 11:37:02 +0530

    issue fixed

--- app/Imports/HtusImport.php
+++ app/Imports/HtusImport.php
@@ -4,6 +4,7 @@ namespace App\Imports;
 
 use App\Models\Htus;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithUpserts;
@@ -25,35 +26,33 @@ class HtusImport implements
      */
     public function model(array $row)
     {
+        Log::debug('Error at ' . $row['start_date_of_validity']);
         return new Htus([
             'ruling_reference' => $row['ruling_reference'],
             'issuing_country' => $row['issuing_country'],
             'start_date_of_validity' => !empty($row['start_date_of_validity'])
-                ? Date::excelToDateTimeObject(
-                    $row['start_date_of_validity']
-                )->format('Y-m-d')
+                ? $this->transformDateTime($row['start_date_of_validity'])
                 : '',
             'end_date_of_validity' => !empty($row['end_date_of_validity'])
-                ? Date::excelToDateTimeObject(
-                    $row['end_date_of_validity']
-                )->format('Y-m-d')
+                ? $this->transformDateTime($row['end_date_of_validity'])
                 : '',
-            'nomenclature_code' => $row['nomenclature_code'],
-            'short_nomenclature_code' => $row['short_nomenclature_code'],
+            'nomenclature_code' => $row['nomenclature_code'] ?? '',
+            'short_nomenclature_code' => $row['short_nomenclature_code'] ?? '',
             'classification_justification' =>
-                $row['classification_justification'],
-            'language' => $row['language'],
-            'place_of_issue' => $row['place_of_issue'],
-            'date_of_issue' => $row['date_of_issue'],
-            'name_address' => $row['name_address'],
-            'description_0f_goods' => $row['description_0f_goods'],
-            'keywords' => $row['keywords'],
-            'eccn' => $row['eccn'],
-            'image_url' => $row['image_url'],
-            'amazon_doc' => $row['amazon_doc'],
-            'chapter_note' => $row['chapter_note'],
-            'comments' => $row['comments'],
-            'image' => $row['image'],
+                $row['classification_justification'] ?? '',
+            'language' => $row['language'] ?? '',
+            'place_of_issue' => $row['place_of_issue'] ?? '',
+            'date_of_issue' => !empty($row['date_of_issue'])
+                ? $this->transformDateTime($row['date_of_issue'])
+                : '',
+            'name_address' => $row['name_address'] ?? '',
+            'description_0f_goods' => $row['description_0f_goods'] ?? '',
+            'keywords' => $row['keywords'] ?? '',
+            'eccn' => $row['eccn'] ?? '',
+            'image_url' => $row['image_url'] ?? '',
+            'amazon_doc' => $row['amazon_doc'] ?? '',
+            'chapter_note' => $row['chapter_note'] ?? '',
+            'comments' => $row['comments'] ?? '',
         ]);
     }
 
@@ -74,11 +73,21 @@ class HtusImport implements
     private function transformDateTime(string $value, string $format = 'd-m-Y')
     {
         try {
+            Log::debug(
+                'Date one ' .
+                    Carbon::instance(
+                        Date::excelToDateTimeObject(intval($value))
+                    )->format($format)
+            );
             return Carbon::instance(
-                Date::excelToDateTimeObject($value)
+                Date::excelToDateTimeObject(intval($value))
             )->format($format);
         } catch (\ErrorException $e) {
-            return Carbon::createFromFormat($format, $value);
+            Log::debug(
+                'Date two ' .
+                    Carbon::createFromFormat($format, $value)->format($format)
+            );
+            return Carbon::createFromFormat($format, $value)->format($format);
         }
     }
 }
