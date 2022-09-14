5d8919bd034a01b02bde1215c169935f4950566e
Sonu Singh <sonu.singh@pairroxz.com>
2022-09-14 10:27:30 +0530

    issue fixed

--- app/Http/Controllers/Admin/ImportController.php
+++ app/Http/Controllers/Admin/ImportController.php
@@ -17,17 +17,24 @@ class ImportController extends Controller
 {
     public function fileImport(Request $request)
     {
-        try{
-            if($request->hasFile('file')){
-                Excel::queueImport(new HtusImport, $request->file('file'));
-                Artisan::call('queue:listen');
-                return back()->with(['message'=>"File has been uploaded successfully. The file is being processed in the background"]);
-            }else{
-                return back()->with(['error_message'=>"Kindly choose file to import."]);
+        try {
+            if ($request->hasFile('file')) {
+                Excel::queueImport(new HtusImport(), $request->file('file'));
+                Artisan::call('queue:listen --timeout=1200');
+                return back()->with([
+                    'message' =>
+                        'File has been uploaded successfully. The file is being processed in the background',
+                ]);
+            } else {
+                return back()->with([
+                    'error_message' => 'Kindly choose file to import.',
+                ]);
             }
-        }catch(Exception $e){
+        } catch (Exception $e) {
             Log::error($e);
-            return back()->with(['error_message'=>"Oops! Something went wrong. Try again."]);
+            return back()->with([
+                'error_message' => 'Oops! Something went wrong. Try again.',
+            ]);
         }
     }
 
@@ -40,116 +47,186 @@ class ImportController extends Controller
                 ->addIndexColumn()
                 ->filter(function ($instance) use ($request) {
                     if ($request->has('search')) {
-                        $instance->where('ruling_reference', 'like', "%{$request->get('search')}%")
-                        ->orWhere('nomenclature_code', 'like', "%{$request->get('search')}%")
-                        ->orWhere('classification_justification', 'like', "%{$request->get('search')}%")
-                        ->orWhere('place_of_issue', 'like', "%{$request->get('search')}%")
-                        ->orWhere('name_address', 'like', "%{$request->get('search')}%")
-                        ->orWhere('description_0f_goods', 'like', "%{$request->get('search')}%")
-                        ->orWhere('keywords', 'like', "%{$request->get('search')}%")
-                        ->orWhere('eccn', 'like', "%{$request->get('search')}%")
-                        ->orWhere('chapter_note', 'like', "%{$request->get('search')}%")
-                        ->orWhere('comments', 'like', "%{$request->get('search')}%");
+                        $instance
+                            ->where(
+                                'ruling_reference',
+                                'like',
+                                "%{$request->get('search')}%"
+                            )
+                            ->orWhere(
+                                'nomenclature_code',
+                                'like',
+                                "%{$request->get('search')}%"
+                            )
+                            ->orWhere(
+                                'classification_justification',
+                                'like',
+                                "%{$request->get('search')}%"
+                            )
+                            ->orWhere(
+                                'place_of_issue',
+                                'like',
+                                "%{$request->get('search')}%"
+                            )
+                            ->orWhere(
+                                'name_address',
+                                'like',
+                                "%{$request->get('search')}%"
+                            )
+                            ->orWhere(
+                                'description_0f_goods',
+                                'like',
+                                "%{$request->get('search')}%"
+                            )
+                            ->orWhere(
+                                'keywords',
+                                'like',
+                                "%{$request->get('search')}%"
+                            )
+                            ->orWhere(
+                                'eccn',
+                                'like',
+                                "%{$request->get('search')}%"
+                            )
+                            ->orWhere(
+                                'chapter_note',
+                                'like',
+                                "%{$request->get('search')}%"
+                            )
+                            ->orWhere(
+                                'comments',
+                                'like',
+                                "%{$request->get('search')}%"
+                            );
                     }
                 })
-                ->addColumn('chapter_note', function($htuses){
-                    $ch_str = "";
-                    if(!empty($htuses->chapter_note)){
+                ->addColumn('chapter_note', function ($htuses) {
+                    $ch_str = '';
+                    if (!empty($htuses->chapter_note)) {
                         $chapter_notes = explode(',', $htuses->chapter_note);
-                        if(is_array($chapter_notes) && !empty($chapter_notes)){
-                            foreach($chapter_notes as $cn){
-                                $ch_str .= "<a href='".$cn."' target='_blank'><i class='uil-invoice'></i></a> &nbsp;&nbsp;";
+                        if (
+                            is_array($chapter_notes) &&
+                            !empty($chapter_notes)
+                        ) {
+                            foreach ($chapter_notes as $cn) {
+                                $ch_str .=
+                                    "<a href='" .
+                                    $cn .
+                                    "' target='_blank'><i class='uil-invoice'></i></a> &nbsp;&nbsp;";
                             }
                         }
                     }
                     return $ch_str;
                 })
-                ->addColumn('amazon_doc', function($htuses){
-                    $ad_str = "";
-                    if(!empty($htuses->amazon_doc)){
+                ->addColumn('amazon_doc', function ($htuses) {
+                    $ad_str = '';
+                    if (!empty($htuses->amazon_doc)) {
                         $amazon_docs = explode(',', $htuses->amazon_doc);
-                        if(is_array($amazon_docs) && !empty($amazon_docs)){
-                            foreach($amazon_docs as $cn){
-                                $ad_str .= "<a href='".$cn."' target='_blank'><i class='uil-invoice'></i></a> &nbsp;&nbsp;";
+                        if (is_array($amazon_docs) && !empty($amazon_docs)) {
+                            foreach ($amazon_docs as $cn) {
+                                $ad_str .=
+                                    "<a href='" .
+                                    $cn .
+                                    "' target='_blank'><i class='uil-invoice'></i></a> &nbsp;&nbsp;";
                             }
                         }
                     }
                     return $ad_str;
                 })
-                ->addColumn('image', function($htuses){
-                    return "<img src='".$htuses->image_url."' height='50' width='50' alt='".$htuses->image."' />";
+                ->addColumn('image', function ($htuses) {
+                    return "<img src='" .
+                        $htuses->image_url .
+                        "' height='50' width='50' alt='" .
+                        $htuses->image .
+                        "' />";
                 })
-                ->addColumn('short_description', function($htuses){
+                ->addColumn('short_description', function ($htuses) {
                     return Str::limit($htuses->short_description);
                 })
-                ->addColumn('action', function($htuses) {
-                    return '<button type="button" class="btn btn-success btn-sm" id="getEditProductData" data-id="'.$htuses->id.'">Edit</button>
-                        <button type="button" data-id="'.$htuses->id.'" class="btn btn-danger btn-sm" id="getDeleteId">Delete</button>
-                        <a class="btn btn-warning btn-sm" href="'.route('getDetail', $htuses->id).'">View</a>';
+                ->addColumn('action', function ($htuses) {
+                    return '<button type="button" class="btn btn-success btn-sm" id="getEditProductData" data-id="' .
+                        $htuses->id .
+                        '">Edit</button>
+                        <button type="button" data-id="' .
+                        $htuses->id .
+                        '" class="btn btn-danger btn-sm" id="getDeleteId">Delete</button>
+                        <a class="btn btn-warning btn-sm" href="' .
+                        route('getDetail', $htuses->id) .
+                        '">View</a>';
                 })
-                ->rawColumns(['image','action','chapter_note','amazon_doc'])
+                ->rawColumns(['image', 'action', 'chapter_note', 'amazon_doc'])
                 ->make(true);
         }
 
         return view('admin.dashboard');
     }
 
-    public function update(Request $request, $id){
-        try{
+    public function update(Request $request, $id)
+    {
+        try {
             $data = Htus::find($id);
-            if(!$data){
-                return response()->json(["status"=> false, "message"=> "Item not found."]);
+            if (!$data) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Item not found.',
+                ]);
             }
-            $data->comments = $request->comments ?? "n\a";
+            $data->comments = $request->comments ?? 'n\a';
             $data->save();
 
-            return response()->json(["status"=> true]);
-
-        }catch(Exception $e){
+            return response()->json(['status' => true]);
+        } catch (Exception $e) {
             Log::error($e);
         }
     }
 
-    public function get(Request $request, $id){
+    public function get(Request $request, $id)
+    {
         $data = Htus::find($id);
-        $html = "";
-        if($data){
+        $html = '';
+        if ($data) {
             $html = '<div class="form-group">';
-            $html .= '<input type="text" class="form-control" id="comments" name="comment" value="'.$data->comments.'" />';
+            $html .=
+                '<input type="text" class="form-control" id="comments" name="comment" value="' .
+                $data->comments .
+                '" />';
             $html .= '</div>';
         }
 
         return response()->json(['html' => $html]);
-
     }
 
-    public function getDetail(Request $request, $id){
+    public function getDetail(Request $request, $id)
+    {
         $data = Htus::find($id);
-        if(!$data){
+        if (!$data) {
             return back();
         }
         return view('admin.item', compact('data'));
-
     }
 
-    public function delete(Request $request, $id){
-
+    public function delete(Request $request, $id)
+    {
         $data = Htus::find($id);
-        if($data){
+        if ($data) {
             $data->delete();
         }
 
-        return response()->json(['html' => ""]);
+        return response()->json(['html' => '']);
     }
 
-    public function removeAll(Request $request){
-        try{
+    public function removeAll(Request $request)
+    {
+        try {
             Htus::query()->delete();
-            return redirect(route('admin.dashboard'))->with(["message"=>"All data removed successfully"]);
-        }catch(Exception $e){
+            return redirect(route('admin.dashboard'))->with([
+                'message' => 'All data removed successfully',
+            ]);
+        } catch (Exception $e) {
             Log::error($e);
-            return back()->with(['error_message'=> "Oops! Something went wrong. Try again."]);
+            return back()->with([
+                'error_message' => 'Oops! Something went wrong. Try again.',
+            ]);
         }
     }
 }
-
