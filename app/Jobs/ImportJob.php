2b285af9c199c3e643d8d6de5a1c4d801a00a0b3
Sonu Singh <sonu.singh@pairroxz.com>
2022-09-27 15:16:41 +0530

    issue fixed

--- app/Jobs/ImportJob.php
+++ app/Jobs/ImportJob.php
@@ -39,67 +39,67 @@ class ImportJob implements ShouldQueue
      */
     public function handle()
     {
-        Excel::import(new HtusImport(), $this->file);
-        // SimpleExcelReader::create($this->file, 'xlsx')
-        //     ->useHeaders([
-        //         'ruling_reference',
-        //         'issuing_country',
-        //         'start_date_of_validity',
-        //         'end_date_of_validity',
-        //         'nomenclature_code',
-        //         'classification_justification',
-        //         'short_nomenclature_code',
-        //         'language',
-        //         'place_of_issue',
-        //         'date_of_issue',
-        //         'name_address',
-        //         'description_0f_goods',
-        //         'keywords',
-        //         'eccn',
-        //         'image_url',
-        //         'amazon_doc',
-        //         'chapter_note',
-        //         'comments',
-        //         'short_description',
-        //     ])
-        //     ->getRows()
-        //     ->each(function (array $rowProperties) {
-        //         Htus::updateOrCreate(
-        //             [
-        //                 'ruling_reference' =>
-        //                     $rowProperties['ruling_reference'],
-        //             ],
-        //             [
-        //                 'ruling_reference' =>
-        //                     $rowProperties['ruling_reference'],
-        //                 'issuing_country' => $rowProperties['issuing_country'],
-        //                 'start_date_of_validity' =>
-        //                     $rowProperties['start_date_of_validity'],
-        //                 'end_date_of_validity' =>
-        //                     $rowProperties['end_date_of_validity'],
-        //                 'nomenclature_code' =>
-        //                     $rowProperties['nomenclature_code'],
-        //                 'classification_justification' =>
-        //                     $rowProperties['classification_justification'],
-        //                 'short_nomenclature_code' =>
-        //                     $rowProperties['short_nomenclature_code'],
-        //                 'language' => $rowProperties['language'],
-        //                 'place_of_issue' => $rowProperties['place_of_issue'],
-        //                 'date_of_issue' => $rowProperties['date_of_issue'],
-        //                 'name_address' => $rowProperties['name_address'],
-        //                 'description_0f_goods' =>
-        //                     $rowProperties['description_0f_goods'],
-        //                 'keywords' => $rowProperties['keywords'],
-        //                 'eccn' => $rowProperties['eccn'],
-        //                 'image_url' => $rowProperties['image_url'],
-        //                 'amazon_doc' => $rowProperties['amazon_doc'],
-        //                 'chapter_note' => $rowProperties['chapter_note'],
-        //                 'comments' => $rowProperties['comments'],
-        //                 'short_description' =>
-        //                     $rowProperties['short_description'],
-        //             ]
-        //         );
-        //     });
+        //Excel::import(new HtusImport(), $this->file);
+        SimpleExcelReader::create($this->file, 'xlsx')
+            ->useHeaders([
+                'ruling_reference',
+                'issuing_country',
+                'start_date_of_validity',
+                'end_date_of_validity',
+                'nomenclature_code',
+                'classification_justification',
+                'short_nomenclature_code',
+                'language',
+                'place_of_issue',
+                'date_of_issue',
+                'name_address',
+                'description_0f_goods',
+                'keywords',
+                'eccn',
+                'image_url',
+                'amazon_doc',
+                'chapter_note',
+                'comments',
+                'short_description',
+            ])
+            ->getRows()
+            ->each(function (array $rowProperties) {
+                Htus::updateOrCreate(
+                    [
+                        'ruling_reference' =>
+                            $rowProperties['ruling_reference'],
+                    ],
+                    [
+                        'ruling_reference' =>
+                            $rowProperties['ruling_reference'],
+                        'issuing_country' => $rowProperties['issuing_country'],
+                        'start_date_of_validity' =>
+                            $rowProperties['start_date_of_validity'],
+                        'end_date_of_validity' =>
+                            $rowProperties['end_date_of_validity'],
+                        'nomenclature_code' =>
+                            $rowProperties['nomenclature_code'],
+                        'classification_justification' =>
+                            $rowProperties['classification_justification'],
+                        'short_nomenclature_code' =>
+                            $rowProperties['short_nomenclature_code'],
+                        'language' => $rowProperties['language'],
+                        'place_of_issue' => $rowProperties['place_of_issue'],
+                        'date_of_issue' => $rowProperties['date_of_issue'],
+                        'name_address' => $rowProperties['name_address'],
+                        'description_0f_goods' =>
+                            $rowProperties['description_0f_goods'],
+                        'keywords' => $rowProperties['keywords'],
+                        'eccn' => $rowProperties['eccn'],
+                        'image_url' => $rowProperties['image_url'],
+                        'amazon_doc' => $rowProperties['amazon_doc'],
+                        'chapter_note' => $rowProperties['chapter_note'],
+                        'comments' => $rowProperties['comments'],
+                        'short_description' =>
+                            $rowProperties['short_description'],
+                    ]
+                );
+            });
     }
 
     public function failed(Exception $e)
