7e476b815288dfeb177e699d7a03c9a81fb76695
Sonu Singh <sonu.singh@pairroxz.com>
2022-09-27 11:41:16 +0530

    issue fixed

--- app/Http/Controllers/Admin/ImportController.php
+++ app/Http/Controllers/Admin/ImportController.php
@@ -27,10 +27,10 @@ class ImportController extends Controller
                     $request->file('file')
                 );
                 // dd($file);
-                Log::info('File Path: ' . Storage::path($file));
+                Log::info('File Path: ' . Storage::get($file));
                 //Excel::import(new HtusImport(), $request->file('file'));
                 //(new HtusImport)->queue($request->file('file'));
-                dispatch(new ImportJob(Storage::path($file)));
+                dispatch(new ImportJob(Storage::get($file)));
                 return back()->with([
                     'message' =>
                         'File has been uploaded successfully. The file is being processed in the background',
