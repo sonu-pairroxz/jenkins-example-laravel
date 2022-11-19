8df2f347ae02898dbd66aa9e56f15e35d8608dcb
Sonu Singh <jadoun91@gmail.com>
2022-11-19 10:05:36 +0530

    changes

--- app/Http/Controllers/Admin/ImportController.php
+++ app/Http/Controllers/Admin/ImportController.php
@@ -34,27 +34,7 @@ class ImportController extends Controller
                 SimpleExcelReader::create(
                     Storage::path($file),
                     'xlsx'
-                )->useHeaders([
-                        'ruling_reference',
-                        'issuing_country',
-                        'start_date_of_validity',
-                        'end_date_of_validity',
-                        'nomenclature_code',
-                        'classification_justification',
-                        'short_nomenclature_code',
-                        'language',
-                        'place_of_issue',
-                        'date_of_issue',
-                        'name_address',
-                        'description_0f_goods',
-                        'keywords',
-                        'eccn',
-                        'image_url',
-                        'amazon_doc',
-                        'chapter_note',
-                        'comments',
-                        'short_description',
-                    ])
+                )
                     ->getRows()
                     ->each(function (array $rowProperties) {
                         Htus::updateOrCreate(
