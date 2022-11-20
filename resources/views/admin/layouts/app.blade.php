578ad37689adc252795b7676911b46f2ed892bcd
Sonu Singh <jadoun91@gmail.com>
2022-11-20 08:57:22 +0530

    pdf viewer

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -73,7 +73,7 @@
 
 {{-- View PDF    --}}
 <div class="modal fade" id="modalpdfviewer" tabindex="-1" role="dialog" aria-labelledby="modalpdfviewer" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="modalpdfviewerTitle"></h5>
